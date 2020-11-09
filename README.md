cca812fd3eecdec93f393f3aa5b4c0ded5bf37e8
nadia-z <33801529+nadia-z@users.noreply.github.com>
2020-11-09 10:10:29 +0100

    Updated LICENSE and README

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-# enduse-sector
+# Enduse Sector Risks Module
+
+This repository contains the code for [*Transition Risks – Enduse Sector Transformation*](https://climatescenarios.org/end-use), a learn module on finance risks related to transitions in the power sector. It is part of the [SENSES Toolkit](https://climatescenarios.org/).
+
+![screenshot of the module](./module.png)
+
+## License
+
+The source code is licensed under the [ISC license](LICENSE.md). Text and graphics are licensed under [Attribution-ShareAlike 4.0 International](https://creativecommons.org/licenses/by-sa/4.0/). For licensing information on datasets please refer to the data sources section. Exceptions are listed below on a per-file basis.
+
+## Data Sources
+
+Data sources and if applicable licenses are listed below on a per-file basis.
+
+- `src/assets/data/CostStructureAgg.csv` `src/assets/data/Investments.csv` `src/assets/data/SecondaryEnergy.csv` `src/assets/data/SecondaryEnergyAndAllCosts.csv` `src/assets/data/SecondaryEnergyAndEmissionCosts.csv`: [CD-LINKS Scenario and Data hosted by IIASA]( https://data.ene.iiasa.ac.at/cd-links/); [Licence for the CD-LINKS scenario ensemble data](https://data.ene.iiasa.ac.at/cd-links/#/license)
+
+## Development
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,191 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SensesMenu :id="'power-sector'"/>
+    <div class="content" :class="mobile ? 'isMobile' : 'isDesktop'" ref="container">
+      <div class="text-wrapper">
+        <h1 class="module-title">Transition risks – Enduse Sector</h1>
+        <p>This is the third chapter of the Finance Risks Pathway.
+          You can select another chapter below.</p>
+      </div>
+      <RiskPathway :mobile="mobile"/>
+      <div class="text-wrapper">
+        <h2 class="chapter-title" id="introduction">
+          What kind of risks are we talking about?
+        </h2>
+        <p>The Enduse sector goes through a transition from Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+          no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua.
+          </p>
+      </div>
+      <div class="text-wrapper">
+        <h2 class="chapter-title-transition">
+          Physical Transition
+        </h2>
+      </div>
+      <div class="vis-wrapper finalenergy">
+        <FinalEnergy :width="width" :height="height" :mobile="mobile"/>
+      </div>
+      <div class="text-wrapper">
+        <h2 class="chapter-title" id="costs">
+          Fuel cost changes
+        </h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+          ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum.</p>
+      </div>
+      <div class="vis-wrapper">
+        <EmiCostsRisk :width="width" :height="height"/>
+      </div>
+      <div class="text-wrapper">
+        <h2 class="chapter-title" id="structure">
+          Investment need
+        </h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+          ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum.</p>
+      </div>
+      <div class="vis-wrapper">
+        <StructureRisk :width="width" :height="height"/>
+      </div>
+      <div class="text-wrapper" id="last-text">
+        <h2 class="chapter-title" id="conclusion">
+          Assessing risks with the help of climate scenarios
+        </h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+          ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum.</p>
+      </div>
+      <SensesMeta :id="'power-sector'"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SensesMenu from 'library/src/components/SensesMenu.vue'
+import RiskPathway from './components/RiskPathway.vue'
+import FinalEnergy from './components/FinalEnergy.vue'
+import EmiCostsRisk from './components/EmiCostsRisk.vue'
+import StructureRisk from './components/StructureRisk.vue'
+import SensesMeta from 'library/src/components/SensesMeta.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SensesMenu,
+    RiskPathway,
+    FinalEnergy,
+    EmiCostsRisk,
+    StructureRisk,
+    SensesMeta
+  },
+  data () {
+    return {
+      width: 0,
+      height: 0
+    }
+  },
+  computed: {
+    mobile () {
+      let isMobile = false
+      if (this.width < 750) { isMobile = true }
+      return isMobile
+    }
+  },
+  methods: {
+    calcSizes () {
+      const { container: el } = this.$refs
+      const totalWidth = el.clientWidth
+      const totalHeight = el.clientHeight || el.parentNode.clientHeight
+      this.width = Math.max(totalWidth, 400)
+      this.height = Math.max(totalHeight, 400)
+    }
+  },
+  mounted () {
+    this.calcSizes()
+    window.addEventListener('resize', this.calcSizes, false)
+  },
+  updated () {
+    this.calcSizes()
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize', this.calcSizes, false)
   }
 }
 </script>
 
 <style lang="scss">
+@import "library/src/style/base.scss";
+@import "library/src/style/variables.scss";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+
+  .content {
+    max-width: 900px;
+    margin: 0 auto;
+
+    .text-wrapper {
+      margin-top: $spacing;
+
+      .module-title {
+        font-size: 40px;
+        margin-bottom: $spacing / 2;
+        padding-top: $spacing;
+      }
+
+      .chapter-title {
+        margin-bottom: $spacing;
+      }
+
+      .chapter-title-transition{
+        margin-bottom: $spacing/4;
+      }
+
+      &#last-text {
+        margin-bottom: 100px;
+      }
+
+      #conclusion {
+        border-top: 1px solid $color-gray;
+        padding-top: 30px;
+      }
+    }
+
+    .vis-wrapper {
+      margin-top: $spacing;
+      // border: 0.5px solid lightblue;
+      height: 100vh;
+
+      &.finalenergy  {
+        height: 100vh;
+        margin-top: $spacing/2;
+      }
+    }
+
+    .module-meta {
+      margin-bottom: $spacing;
+    }
+
+    &.isMobile {
+      .text-wrapper {
+        padding: 0 20px;
+      }
+    }
+  }
 }
 </style>
